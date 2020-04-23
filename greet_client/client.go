9e8acc77003a0d429df378749ef502b4df3b3484
nexlight101 <henniejpienaar@gmail.com>
2020-04-23 14:58:16 +0200

    Implemented Unary Greet RPC

--- greet_client/client.go
+++ greet_client/client.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/nexlight101/greet/greetpb"
+	"google.golang.org/grpc"
+)
 
 func main() {
 	fmt.Println("Hello I am a gRPC client")
+
+	// Create connection to the server
+	options := grpc.WithInsecure()
+	cc, err := grpc.Dial("localhost:50051", options)
+	if err != nil {
+		log.Fatalf("Could not connect: %v\n", err)
+	}
+
+	// CLose the connection at exit
+	defer cc.Close()
+
+	// Establish a new client
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("Client activated: %v\n", c)
+
+	// Call doUnary Greet RPC
+	doUnary(c)
+}
+func doUnary(c greetpb.GreetServiceClient) {
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			First_Name: "Hendrink",
+			Last_Name:  "Pienaar",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("Cannot get response from server: %v\n", err)
+	}
+	fmt.Println(res.GetResult())
 }
--- greet_server/server.go
+++ greet_server/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
@@ -9,14 +10,28 @@ import (
 	"google.golang.org/grpc"
 )
 
+// Create a server type
+type server struct{}
+
+// Implemente Greet method
+func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.GreetResponse, error) {
+	// Extract the name from the request
+	fullname := req.GetGreeting().GetFirst_Name() + " " + req.GetGreeting().GetLast_Name()
+
+	// Responde with a greeting containing the name
+	result := "Hello " + fullname
+	res := &greetpb.GreetResponse{
+		Result: result,
+	}
+	return res, nil
+}
+
 func main() {
-	// Create a server type
-	type server struct{}
 
 	fmt.Println("Hello from grpc")
 
 	//Create a listener
-	lis, err := net.Listen("tcp", "0.0.0.0:500051")
+	lis, err := net.Listen("tcp", "0.0.0.0:50051")
 	// handle error
 	if err != nil {
 		log.Fatalf("Failed to listen: %v", err)
